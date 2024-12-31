7c106220a11fb6f70a94c9ff9052274b998e671c
zorgms <100448801+zorgms@users.noreply.github.com>
2024-12-31 18:23:36 +0100

    updated 12V to alert watching

--- plugins/booster_12V/booster_12V.js
+++ plugins/booster_12V/booster_12V.js
@@ -11,29 +11,24 @@ var init_usrcfg = OvmsConfig.Get("usr","12v.init", "no");
 
 if(init_usrcfg == "no"){
     OvmsConfig.Set("usr", "12v.init", "yes");      // init usr config
-    OvmsConfig.Set("usr", "12v.time_1", "1900");   // schedule time 1 format 1400 for 14:00 / 2 p.m. o'clock 
-    OvmsConfig.Set("usr", "12v.time_2", "0500");   // schedule time 2 format 1400 for 14:00 / 2 p.m. o'clock 
     OvmsConfig.Set("usr", "12v.charging", "yes");  // activate 12V charging events
     OvmsConfig.Set("usr", "12v.ps_time_1", "0");       // PubSub ID time 1 subscription
-    OvmsConfig.Set("usr", "12v.ps_time_2", "0");       // PubSub ID time 2 subscription
     OvmsConfig.Set("usr", "12v.ps_booster_2", "0");    // PubSub ID 2x booster subscription
 }
 
 var state_12v = {
   init: false,
+  alert: false,
   time_1: OvmsConfig.Get("usr","12v.ps_time_1", "0"),       // time 1 subscription
-  time_2: OvmsConfig.Get("usr","12v.ps_time_2", "0"),       // time 2 subscription
   booster_2: OvmsConfig.Get("usr","12v.ps_booster_2", "0"), // 2x booster subscription
 };
 
 // delete old events
 PubSub.unsubscribe(state_12v.time_1);
-PubSub.unsubscribe(state_12v.time_2);
 PubSub.unsubscribe(state_12v.booster_2);
 
 // get value
 var time_on_1 = 'clock.' + OvmsConfig.Get("usr","12v.time_1", "1900"); 
-var time_on_2 = 'clock.' + OvmsConfig.Get("usr","12v.time_2", "0500");
 var charging_12v = OvmsConfig.Get("usr", "12v.charging", "yes");
 
 function veh_on() {
@@ -54,7 +49,8 @@ function alert_12v() {
 function charge_12v_check() {
 
   if(!veh_on() && !charging())  {
-      if (alert_12v()) {
+      if (alert_12v() && !state_12v.alert) {
+        state_12v.alert = true;
         OvmsVehicle.ClimateControl("on");
         state_12v.booster_2 = PubSub.subscribe('ticker.60',charge_12v_boost_2);
         OvmsConfig.Set("usr", "12v.ps_booster_2", state_12v.booster_2);
@@ -65,11 +61,13 @@ function charge_12v_check() {
 // scheduled booster time add a second booster time for 10 min. booster
 function charge_12v_boost_2() {
   if(!veh_on() && !charging() && !veh_hvac()) {
-      OvmsVehicle.ClimateControl("on");
-      PubSub.unsubscribe(state_12v.booster_2);
+    OvmsVehicle.ClimateControl("on");
+    state_12v.alert = false;
+    PubSub.unsubscribe(state_12v.booster_2);
   }
   if(veh_on()) {
-      PubSub.unsubscribe(state_12v.booster_2);
+    state_12v.alert = false;
+    PubSub.unsubscribe(state_12v.booster_2);
   }
 }
 
@@ -78,12 +76,8 @@ if(!state_12v.init){
   state_12v.init = true;
   state_12v.time_1 = PubSub.subscribe('clock.0001',veh_on);
   PubSub.unsubscribe(state_12v.time_1);
-  state_12v.time_2 = PubSub.subscribe('clock.0001',veh_on);
-  PubSub.unsubscribe(state_12v.time_2);
   state_12v.booster_2 = PubSub.subscribe('clock.0001',veh_on);
   PubSub.unsubscribe(state_12v.booster_2);
-  state_12v.time_1 = PubSub.subscribe(time_on_1,charge_12v_check);
-  state_12v.time_2 = PubSub.subscribe(time_on_2,charge_12v_check);
+  state_12v.time_1 = PubSub.subscribe('vehicle.alert.12v.on',charge_12v_check);
   OvmsConfig.Set("usr", "12v.ps_time_1", state_12v.time_1);
-  OvmsConfig.Set("usr", "12v.ps_time_2", state_12v.time_2);
 }
